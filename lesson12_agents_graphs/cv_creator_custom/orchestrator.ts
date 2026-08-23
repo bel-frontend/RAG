21e476979ed253b0119e0ce588950f19f6f5c6d8
piotrazsko <serj432@yandex.ru>
2026-08-23 18:51:59 +0200

    feat(cv-creator): loop extraction for required data

--- lesson12_agents_graphs/cv_creator_custom/orchestrator.ts
+++ lesson12_agents_graphs/cv_creator_custom/orchestrator.ts
@@ -3,9 +3,7 @@ import { StateGraph, MemorySaver } from '@langchain/langgraph';
 import { CV_Structure,createCVFromText , vacancyText, candidateText} from './dataPrepare';
 import { log } from 'console';
 
-const ResearchState = Annotation.Root({
 
-});
 
 
 const CVState = Annotation.Root({
@@ -14,6 +12,7 @@ const CVState = Annotation.Root({
   cv: Annotation<CV_Structure | null>({ reducer: (_, u) => u, default: () => null }),
   questionsToUser: Annotation<string[]>({ reducer: (_, u) => u, default: () => [] }),
   userAnswer: Annotation<string>({ reducer: (_, u) => u, default: () => "" }),
+  requiredData:Annotation<string[]>({reducer:(_,u)=>u,default:()=>[]})
 });
 
 type CVStateType = typeof CVState.State
@@ -21,12 +20,12 @@ type CVStateType = typeof CVState.State
 async function extractNode(state: CVStateType) {
   const result = await createCVFromText("", state.candidateText, state.vacancyText);
   log('RESULT:',result)
-  return { cv: result.data.cv, questionsToUser: result.data.questionsToUser };
+  return { cv: result.data.cv, questionsToUser: result.data.questionsToUser, requiredData:result?.data?.meta?.requiredData };
 }
 
 
 
-async function askUser(state: CVStateType) {
+async function askUserNode(state: CVStateType) {
   return {}
 }
 
@@ -35,14 +34,15 @@ function afterExtract(state: CVStateType): "askUser" | "__end__" {
   return state.questionsToUser.length > 0 ? "askUser" : "__end__";
 }
 
-const workflow = new StateGraph(ResearchState)
-  .addNode('extract', extractNode)
-  .addNode('askUser',askUser)
+const workflow = new StateGraph(CVState)
+  .addNode("extract", extractNode)
+  .addNode("askUser", askUserNode)
   .addEdge("__start__", "extract")
-  .addConditionalEdges('extract', afterExtract, {
+  .addConditionalEdges("extract", afterExtract, {
     askUser: "askUser",
-       __end__: "__end__",
+    __end__: "__end__",
   })
+  .addEdge("askUser", "extract");
 
 
 
